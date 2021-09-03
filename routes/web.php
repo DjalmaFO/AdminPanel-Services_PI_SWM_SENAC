201f31b769ecb698075468630facef8a2a8b836b
DjalmaFO <53957337+DjalmaFO@users.noreply.github.com>
2021-09-03 14:23:58 -0300

    :rocket: Inserido rotas para produto

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProdutoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/produto', [ProdutoController::class, 'index'])->name('produto.index');
+Route::get('/produto/buscar/{p}', [ProdutoController::class, 'buscar'])->name('produto.find');
+Route::get('/produto/criar', [ProdutoController::class, 'criar'])->name('produto.create');
+Route::post('/produto/inserir/{produto}', [ProdutoController::class, 'inserir'])->name('produto.insert');
+Route::get('/produto/visualizar/{id}', [ProdutoController::class, 'visualizar'])->name('produto.show');
+Route::get('/produto/editar/{id}', [ProdutoController::class, 'editar'])->name('produto.edit');
+Route::post('/produto/alterar/{produto}', [ProdutoController::class, 'alterar'])->name('produto.update');
+Route::post('/produto/inativar/{id}', [ProdutoController::class, 'inativar'])->name('produto.inativar');
