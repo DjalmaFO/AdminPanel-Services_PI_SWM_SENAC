e5522be81f80da43a8a922f83b662e43a05f2159
Djalma.Oliveira <djalma.oliveira@intecomlogistica.com.br>
2021-09-07 20:56:59 -0300

    :rocket: scripts de insert de produtos e categorias

---
+++ database/sql-mysql/inserir-categorias.sql
@@ -0,0 +1,25 @@
+-- use geek_style;
+
+insert into categorias(nm_categoria) values ('Categoria 1');
+
+insert into categorias(nm_categoria) values ('Categoria 2');
+
+insert into categorias(nm_categoria) values ('Categoria 3');
+
+insert into categorias(nm_categoria) values ('Categoria 4');
+
+insert into categorias(nm_categoria) values ('Categoria 5');
+
+insert into categorias(nm_categoria) values ('Categoria 6');
+
+insert into categorias(nm_categoria) values ('Categoria 7');
+
+-- select * from categorias
+
+select 
+	p.nm_produto produto, 
+    p.desc_produto descricao, 
+    p.qtd_produto quantidade, 
+    c.nm_categoria categoria
+from produtos p
+left join categorias c on c.id = p.id_categoria
--- database/sql-mysql/inserir-produtos.sql
+++ database/sql-mysql/inserir-produtos.sql
@@ -1,3 +1,51 @@
 -- use geek_style;
 
-insert into produtos ()values();
+insert into produtos (
+	nm_produto, desc_produto, vl_produto, id_categoria, qtd_produto
+)values(
+	'Produto 1', 'Inserção em massa de produtos', '199.90', 1, 5
+);
+
+insert into produtos (
+	nm_produto, desc_produto, vl_produto, id_categoria, qtd_produto
+)values(
+	'Produto 2', 'Inserção em massa de produtos', '199.90', 1, 5
+);
+
+insert into produtos (
+	nm_produto, desc_produto, vl_produto, id_categoria, qtd_produto
+)values(
+	'Produto 3', 'Inserção em massa de produtos', '199.90', 2, 5
+);
+
+insert into produtos (
+	nm_produto, desc_produto, vl_produto, id_categoria, qtd_produto
+)values(
+	'Produto 4', 'Inserção em massa de produtos', '199.90', 3, 5
+);
+
+insert into produtos (
+	nm_produto, desc_produto, vl_produto, id_categoria, qtd_produto
+)values(
+	'Produto 5', 'Inserção em massa de produtos', '199.90', 1, 5
+);
+
+insert into produtos (
+	nm_produto, desc_produto, vl_produto, id_categoria, qtd_produto
+)values(
+	'Produto 6', 'Inserção em massa de produtos', '199.90', 1, 5
+);
+
+insert into produtos (
+	nm_produto, desc_produto, vl_produto, id_categoria, qtd_produto
+)values(
+	'Produto 7', 'Inserção em massa de produtos', '199.90', 2, 5
+);
+
+insert into produtos (
+	nm_produto, desc_produto, vl_produto, id_categoria, qtd_produto
+)values(
+	'Produto 8', 'Inserção em massa de produtos', '199.90', 3, 5
+);
+
+-- select *from produtos;
