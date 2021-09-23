cda95e5bfd5f0c6ebf305aacd773cd46d9464e1c
DjalmaFO <53957337+DjalmaFO@users.noreply.github.com>
2021-09-23 11:15:12 -0300

    :rocket: Adicionado model, migration e controller para tabela de produtos dos pedidos

---
+++ app/Http/Controllers/TbExplodeProdutosPedidoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TbExplodeProdutosPedido;
+use Illuminate\Http\Request;
+
+class TbExplodeProdutosPedidoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\TbExplodeProdutosPedido  $tbExplodeProdutosPedido
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TbExplodeProdutosPedido $tbExplodeProdutosPedido)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\TbExplodeProdutosPedido  $tbExplodeProdutosPedido
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TbExplodeProdutosPedido $tbExplodeProdutosPedido)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\TbExplodeProdutosPedido  $tbExplodeProdutosPedido
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TbExplodeProdutosPedido $tbExplodeProdutosPedido)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\TbExplodeProdutosPedido  $tbExplodeProdutosPedido
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TbExplodeProdutosPedido $tbExplodeProdutosPedido)
+    {
+        //
+    }
+}
