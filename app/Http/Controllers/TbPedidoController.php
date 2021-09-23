46298813ddfe11f93d7d8a1d8482ae1afef4266d
DjalmaFO <53957337+DjalmaFO@users.noreply.github.com>
2021-09-23 10:12:38 -0300

    :rocket: Inserido migrate, model, e controllers para tabela de pedidos

---
+++ app/Http/Controllers/TbPedidoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TbPedido;
+use Illuminate\Http\Request;
+
+class TbPedidoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\TbPedido  $tbPedido
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TbPedido $tbPedido)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\TbPedido  $tbPedido
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TbPedido $tbPedido)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\TbPedido  $tbPedido
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TbPedido $tbPedido)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\TbPedido  $tbPedido
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TbPedido $tbPedido)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_09_23_095910_create_tb_pedidos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTbPedidosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tb_pedidos', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_user')->references('users')->on('id');
+            $table->string('status')->default('P'); // Pendente
+            $table->string('observação')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tb_pedidos');
+    }
+}
