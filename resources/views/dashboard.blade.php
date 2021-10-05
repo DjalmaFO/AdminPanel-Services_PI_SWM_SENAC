814230563a3e1a6f6b64b3ed247c915fb7bf607a
DjalmaFO <53957337+DjalmaFO@users.noreply.github.com>
2021-10-05 12:51:41 -0300

    :rocket: Inserido layout para telas do painel admnistrativo

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,11 @@
-<x-app-layout>
+@extends('layouts.dash')
+
+@section('corpo-dash')
+    <h1>Implementação do dashboard</h1>
+@endsection
+
+
+{{-- <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
@@ -12,4 +19,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</x-app-layout> --}}
