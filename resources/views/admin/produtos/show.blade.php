1a0d9014d74a372ca991d06661862d664fcc499d
DjalmaFO <53957337+DjalmaFO@users.noreply.github.com>
2021-10-06 15:17:54 -0300

    :rocket: Alterado estrutura da pagina para que possa ser enquadrada dentro do layout do dashborad

--- resources/views/admin/produtos/show.blade.php
+++ resources/views/admin/produtos/show.blade.php
@@ -1,35 +1,27 @@
-<!DOCTYPE html>
-<html lang="pt-bt">
-<head>
-    <meta charset="UTF-8">
-    <title>Geek Style</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-</head>
-<body>
-    <main class="container my-4">
-        @if(session()->has('success'))
-            <div class="alert alert-success" role="alert">{{ session()->get('success') }}</div>
-        @endif
-        @if(session()->has('error'))
-            <div class="alert alert-danger" role="alert">{{ session()->get('error') }}</div>
-        @endif
-        <div class="row text-center">
-            <img src="{{ asset('storage/'.$produto->img_produto)}}" alt="Imagem do produto {{$produto->nm_produto}}">
+@extends('layouts.dash')
+@section('corpo-dash')
+    <div class="row justify-content-center mt-3">
+        <img class="col-lg-3 col-md-4 col-sm-6 col-xs-8" src="{{ asset('storage/' . $produto->img_produto) }}"
+            alt="Imagem do produto {{ $produto->nm_produto }}">
+    </div>
+    <div class="row justify-content-center mt-3">
+        <div class="justify-content-start">
+            <h2>{{ $produto->nm_produto }}</h2>
+            <span class="h4 d-block">R$ {{ $produto->vl_produto }}</span>
+            <span class="h4 d-block">Unidades: {{ $produto->qtd_produto }}</span>
+            <span class="h4 d-block">Categoria: {{ $categoria->nm_categoria }}</span>
+            <span class="h4 d-block">Descrição</span>
+            <p>
+                {{$produto->desc_produto}}
+            </p>
         </div>
-        <div class="row text-center">
-            <div class="text-center">
-                <h2>{{$produto->nm_produto}}</h2>
-                <p>{{ $produto->desc_produto }}</p>
-                <span class="h4 d-block">R$ {{$produto->vl_produto}}</span>
-                <span class="h4 d-block">Unidades: {{$produto->qtd_produto}}</span>
-                <span class="h4 d-block">Categoria: {{$categoria->nm_categoria}}</span>
-            </div>
-            <div class="text-center mt-4">
-                <a href="{{route('adm.produtos.index')}}" class="btn btn-primary btn-md">Voltar</a>
-            </div>
+    </div>
+    <div class="row justify-content-center mt-3">
+        <div class="col-lg-1 col-md-2 col-sm-3 col-xs-4 px-1">
+            <a href="{{ route('adm.produtos.index') }}" class="btn btn-primary btn-md px-3">Voltar</a>
         </div>
-        <div class="mt-4">
+        <div class="col-lg-1 col-md-2 col-sm-3 col-xs-4 px1">
+            <a href="{{ Route('adm.produto.edit', $produto->id) }}" class="btn btn-warning btn-md px-3">Editar</a>
         </div>
-    </main>
-</body>
-</html>
+    </div>
+@endsection
